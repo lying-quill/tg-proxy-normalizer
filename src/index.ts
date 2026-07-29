b98656c18130b8ae69042f05e8223d7e308eac4b
Lying Quill <289309923+lying-quill@users.noreply.github.com>
2026-07-29 15:36:41 +0330

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,77 @@
+import type {
+	APIMethodParams,
+	APIMethods,
+	TelegramAPIResponse,
+	TelegramUpdate,
+} from "@gramio/types";
+import { env } from "cloudflare:workers";
+import { fixLink } from "./lib";
+
+const TOKEN = env.TG_BOT_TOKEN;
+const WEBHOOK_ENDPOINT = `/webhook/${TOKEN}`;
+const TBA_BASE_URL = "https://api.telegram.org/bot";
+
+const api = new Proxy({} as APIMethods, {
+	get:
+		<T extends keyof APIMethods>(_target: APIMethods, method: T) =>
+		async (params: APIMethodParams<T>) => {
+			const response = await fetch(`${TBA_BASE_URL}${TOKEN}/${method}`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(params),
+			});
+
+			const data = (await response.json()) as TelegramAPIResponse;
+
+			if (!data.ok)
+				// ><>
+				throw new Error(`Some error occurred in ${method}`);
+
+			return data.result;
+		},
+});
+
+export default {
+	async fetch(req, _, ctx) {
+		const { pathname: path } = new URL(req.url);
+
+		if (path === WEBHOOK_ENDPOINT && req.method === "POST") {
+			const update = await req.json<TelegramUpdate>();
+
+			const message = update.message;
+			const text = message?.text;
+			const entities = message?.entities;
+
+			if (message && text && entities) {
+				const fixedLinks: string[] = [];
+
+				for (const et of entities) {
+					if (et.type !== "text_link") continue;
+
+					try {
+						fixedLinks.push(
+							// this will throw errors on invalid links
+							fixLink(et.url!),
+						);
+					} catch {}
+				}
+
+				if (fixedLinks.length) {
+					// wait for this api call to finish
+					ctx.waitUntil(
+						api.sendMessage({
+							chat_id: message.chat.id,
+							text: fixedLinks.join("\n"),
+						}),
+					);
+				}
+			}
+
+			return new Response(null, { status: 204 });
+		}
+
+		return new Response("nya >.<", { status: 200 });
+	},
+} satisfies ExportedHandler<Env>;
---
+++ src/lib.ts
@@ -0,0 +1,66 @@
+/**
+ * Normalizes the given MTProxy's secret so it would become compatible
+ * with TDLib.
+ *
+ * @param secret Must be hexadecimal or base64url ascii string
+ *
+ * @returns Hex representation of the secret
+ *
+ * @throws {SyntaxError} If secret is neither Hex nor Base64
+ * @throws {Error} If secret is too short
+ */
+export function fixSecret(secret: string): string {
+	// oxlint-disable-next-line require-unicode-regexp
+	const isHex = /^[0-9a-fA-F]$/.test(secret) && !(secret.length & 1);
+
+	const bytes = isHex
+		? Uint8Array.fromHex(secret)
+		: Uint8Array.fromBase64(secret, { alphabet: "base64url" });
+
+	// TGAndroid does not check for length
+	// if secret doesn't start with 0xEE or 0xDD,
+	// which i believe leads to illegal memory access.
+	if (bytes.length < 16)
+		throw new Error("Secret must be at least 16 bytes long");
+
+	const firstByte = bytes.at(0);
+	switch (firstByte) {
+		// 18 to 199 bytes
+		case 0xee:
+			return bytes
+				.slice(
+					0,
+					// the upper bounds limit is not checked in TGAndroid,
+					// i think?
+					Math.max(bytes.length, 199),
+				)
+				.toHex();
+
+		// 17 bytes
+		case 0xdd:
+			return bytes.slice(0, 17).toHex();
+	}
+
+	// 16 bytes
+	return bytes.slice(0, 16).toHex();
+}
+
+export function fixLink(link: string): string {
+	const { searchParams } = new URL(link),
+		port = searchParams.get("port"),
+		secret = searchParams.get("secret"),
+		server = searchParams.get("server");
+
+	if (!server || !port || !secret)
+		throw new Error(
+			"Invalid proxy link, missing server, port or secret query parameters",
+		);
+
+	const newParams = new URLSearchParams({
+		server,
+		port: (Number.parseInt(port, 10) & 0xffff).toString(10),
+		secret: fixSecret(secret),
+	});
+
+	return `https://t.me/proxy?${newParams.toString()}`;
+}
